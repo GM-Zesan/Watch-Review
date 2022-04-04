88e83455fb1c55c9bec175f8dbaf76519501053a
Zesan <gmzesan7767@gmail.com>
2022-04-05 01:46:30 +0600

    Answaring question done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,43 @@
 import React from "react";
-
+import "./Blog.css";
 const Blogs = () => {
     return (
-        <div>
-            <h2>Blogs</h2>
+        <div className="container answering my-5 text center">
+            <h3>1. What is Context API ?</h3>
+            <p>
+                Prop drilling refers to the process of sending props from a
+                higher-level component to a lower-level component.Prop drilling
+                is the unofficial term for passing data through several nested
+                children components.The Context API is a React structure. It
+                assists in solving prop-drilling from all levels of your
+                application.Context is primarily used when some data needs to be
+                accessible by many components at different nesting levels.
+            </p>
+            <h3>2. What is semantic tag ?</h3>
+            <p>
+                A semantic element clearly describes its meaning to both the
+                browser and the developer.HTML5 semantic tags define the purpose
+                of the element.By using semantic markup, you help not only the
+                browser understand the meaning of the content but also search
+                engines to read the page and find the required information
+                faster.Semantic element help us think about the structure of our
+                dynamic data.Elements such as header, footer, and article are
+                all considered semantic.
+            </p>
+            <h3>3. Inline VS Inline-Block VS Block.</h3>
+            <p>
+                <strong>Inline :</strong> Inline elements appears on the same
+                line. Inline elements do NOT start on a new line and only takes
+                up as much width as its content. So, if you try to set any width
+                and height, it will have NO effects. <br />
+                <strong>Inline-Block :</strong> It’s essentially the same thing
+                as inline, except that you can set height and width values.
+                <br />
+                <strong>Block :</strong> Block starts on a NEW line and takes up
+                the full width available.So that means block elements will
+                occupy the entire width of its parent element.We can also set
+                height and width values.
+            </p>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,13 +14,15 @@ const Home = () => {
         <div className="container">
             <div className="hero-section row mt-4">
                 <div className="col-md-8">
-                    <h1>Your Best Phone</h1>
+                    <p>From,</p>
+                    <h1>Watch Reviewer</h1>
+                    <p>you can choose</p>
+                    <h1>Your Best Watch</h1>
                     <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Cumque fuga temporibus recusandae, facere nostrum
-                        pariatur asperiores quia nam sequi ipsam odit earum
-                        voluptates suscipit similique soluta magni quae a
-                        dolorem dolore eum atque.
+                        Watch blogs are a great way for someone with a love for
+                        watches to share their passion with the world. Watch
+                        bloggers interact with their favorite watch styles,
+                        trends, and ideas of the timepiece world.
                     </p>
                 </div>
                 <div className="col-md-4">
@@ -28,7 +30,7 @@ const Home = () => {
                 </div>
             </div>
             <div className="review-section my-5">
-                <h2 className="mb-5">Customers Review</h2>
+                <h2 className="mb-5">Customers Reviews</h2>
                 <div className="review-container row">
                     {homeReviews.map((review) => (
                         <Review key={review.id} review={review}></Review>
